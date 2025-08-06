704f9c2ac2fa930481c83f21bb46a82b029c10f0
Desai-Vedant <vedant.desai@ncircletech.com>
2025-08-06 14:40:38 +0530

    Cors update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({origin: process.env.CORS_ORIGIN}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
