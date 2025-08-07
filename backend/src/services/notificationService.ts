c50fe93dee5754a8736c140ed0e67883a6c5f514
Desai-Vedant <vedant.desai@ncircletech.com>
2025-08-07 14:23:45 +0530

    Fixed Time Zone issue

--- backend/src/services/notificationService.ts
+++ backend/src/services/notificationService.ts
@@ -6,8 +6,12 @@ import '../models/User';
 // Function to send notifications
 export async function sendNotifications() {
   try {
+    const fmt = new Intl.DateTimeFormat(
+      'en-US', 
+      { timeZone: 'Asia/Kolkata', hour: '2-digit', minute: '2-digit', hour12: false }
+    )
     const now = new Date();
-    const currentTime = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`;
+    const currentTime = fmt.format(now);
 
     // Find all active notifications scheduled for current time
     const notifications = await Notification.find({
